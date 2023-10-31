577384ae58f778a003a3983bfcdd3e41f2bbd0e3
sloev <johannes.valbjorn@gmail.com>
2023-10-31 11:29:45 +0100

    implement map and store

--- saysheap/src/App.vue
+++ saysheap/src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
 import NavBar from "./components/NavBar.vue"
+import { onMounted } from 'vue'
+import { useSheepStore } from './store/store'
+
+const store = useSheepStore();
 
 
+
+onMounted(() => {
+  setTimeout(() => {
+    store.setFollowMyLocation(true)
+
+  }, 500);
+})
 
 </script>
 
--- saysheap/src/views/MapView.vue
+++ saysheap/src/views/MapView.vue
@@ -1,20 +1,18 @@
-
-
 <script setup>
-import Banner from "../components/Banner.vue"
-import 'leaflet/dist/leaflet.css';
-import { ref, onMounted } from 'vue'
+import Banner from "../components/Banner.vue";
+import "leaflet/dist/leaflet.css";
+import { ref, watch, onMounted } from "vue";
 
-import LookingIcon from "../components/icons/icon.looking.gif"
-import LocationOnIcon from "../components/icons/icon.location.on.gif"
-import LocationOffIcon from "../components/icons/icon.location.off.gif"
-import WolfMarkerIcon from "../components/icons/icon.wolfmarker.png"
+import LookingIcon from "../components/icons/icon.looking.gif";
+import LocationOnIcon from "../components/icons/icon.location.on.gif";
+import LocationOffIcon from "../components/icons/icon.location.off.gif";
+import WolfMarkerIcon from "../components/icons/icon.wolfmarker.png";
 
+import { useSheepStore } from "../store/store";
 
+const store = useSheepStore();
 
-
-
-import L from 'leaflet';
+import L from "leaflet";
 
 var wolfMarkerIcon = L.icon({
   iconUrl: WolfMarkerIcon,
@@ -23,101 +21,86 @@ var wolfMarkerIcon = L.icon({
   popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor,
 });
 
-import * as protomapsL from 'protomaps-leaflet';
+import * as protomapsL from "protomaps-leaflet";
 
 let map = null;
-let follow = ref(true)
-let watchId = null;
-let zoomLevel = 16
-let wolfMarker = null;
-let currentLocation = ref(new L.LatLng(55.05982, 10.60677));
-let initialized = ref(false);
+let zoomLevel = 16;
+let wolfMarker = new L.Marker(store.currentLocation, { icon: wolfMarkerIcon });
 
-onMounted(() => {
+const panToPosition = () => {
+  console.warn("panning");
+  wolfMarker.setLatLng(store.currentLocation);
+  map.setView(store.viewPosition, map.getZoom(), {
+    animate: true,
+    pan: {
+      duration: 1,
+    },
+  });
+};
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let latitude = position.coords.latitude;
-      let longitude = position.coords.longitude;
-      map = L.map('map', { zoomControl: false })
-      currentLocation.value = new L.LatLng(latitude, longitude)
+const setupMap = () => {
+  map = L.map("map", { zoomControl: false });
 
-      map.setView(currentLocation.value, zoomLevel);
-      wolfMarker = new L.Marker(currentLocation.value, { icon: wolfMarkerIcon });
+  map.setView(store.viewPosition, zoomLevel);
 
-      var layer = protomapsL.leafletLayer({ url: 'https://api.protomaps.com/tiles/v2/{z}/{x}/{y}.pbf?key=7120db229cd222aa' })
-      layer.addTo(map)
-      map.addLayer(wolfMarker);
+  var layer = protomapsL.leafletLayer({
+    url: "https://api.protomaps.com/tiles/v2/{z}/{x}/{y}.pbf?key=7120db229cd222aa",
+  });
+  layer.addTo(map);
+  map.addLayer(wolfMarker);
 
-      layer.addInspector(map)
-      watchId = navigator.geolocation.watchPosition(panToPosition, (error) => {
-        console.warn(error);
-      }, { enableHighAccuracy: true });
-      initialized.value = true;
+  layer.addInspector(map);
 
-    })
-
-  }
-})
-
-const panToPosition = (position) => {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  currentLocation.value = new L.LatLng(latitude, longitude)
-  map.setView(currentLocation.value, map.getZoom(), {
-    "animate": true,
-    "pan": {
-      "duration": 2
-    }
+  map.on("moveend", function (e) {
+    store.setBoundingBox(map.getBounds());
+  });
+  map.on("dragend", function (event) {
+    store.setBoundingBox(map.getBounds());
+    var latlng = map.getCenter();
+    store.setViewPosition(latlng);
   });
 
-  console.warn(Date.now(), latitude, longitude)
+  watch(() => store.currentLocation, panToPosition);
+};
 
-}
-
-const toggleFollow = () => {
-  follow.value = !follow.value
-  if (follow.value) {
-    if (watchId != null) {
-      navigator.geolocation.clearWatch(watchId);
-    }
-    watchId = navigator.geolocation.watchPosition(panToPosition, (error) => {
-      console.warn(error);
-    }, { enableHighAccuracy: true });
-    if (currentLocation != null) {
-      wolfMarker = new L.Marker(currentLocation.value, { icon: wolfMarkerIcon });
-
-      map.addLayer(wolfMarker);
-    }
+const triggerPan = () => {
+  if (store.followMyLocation) {
+    store.setViewPosition(store.currentLocation)
+    panToPosition();
   }
-  else {
-    if (wolfMarker != null) {
-      map.removeLayer(wolfMarker);
-      wolfMarker.value = null;
+};
 
-    }
-  }
-}
+onMounted(() => {
+  setupMap();
+});
 
 
+
+watch(() => store.followMyLocation, triggerPan);
 
 </script>
 
 <template>
-  <div>
+  <div class="map-container">
+    <div id="map"></div>
+
     <transition name="looking-fade">
-      <img class="looking" v-if="!initialized" :src="LookingIcon" alt="" />
+      <img class="looking" v-if="!store.gpsInitialized" :src="LookingIcon" alt="" />
     </transition>
 
-
-    <div id="map"></div>
     <Banner></Banner>
 
-    <div class="follow" @click="toggleFollow">
-      <img v-if="follow" :src="LocationOnIcon" alt="" />
+    <div
+      class="follow"
+      @click="
+        () => {
+          store.setFollowMyLocation(!store.followMyLocation);
+        }
+      "
+    >
+      <img v-if="store.followMyLocation" :src="LocationOnIcon" alt="" />
       <img v-else :src="LocationOffIcon" alt="" />
     </div>
-
   </div>
 </template>
 
@@ -130,23 +113,23 @@ const toggleFollow = () => {
 .looking-fade-enter,
 .looking-fade-leave-to
 
-/* .fade-leave-active below version 2.1.8 */
-  {
+/* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
 
 .looking {
-  position: fixed;
-  top: 5em;
-  width: 100%;
+  position: absolute;
+  height: 100%;
   z-index: 1;
 }
 
 .follow {
-  position: fixed;
+  position: absolute;
+
+  right: 2em;
   bottom: 12em;
-  right: 5em;
-  z-index: 10;
+
+  z-index: 2;
 }
 
 .follow img {
@@ -154,8 +137,17 @@ const toggleFollow = () => {
 }
 
 #map {
-  height: 100vh;
-  margin: 0px;
+  position: absolute;
+
+  height: 100%;
+  width: 100%;
   z-index: 0;
+}
+.map-container {
+  position: relative;
+
+  max-width: 600px;
+  width: 100%;
+  height: 100%;
 }
 </style>
